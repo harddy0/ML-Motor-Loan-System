376bbb168cd9a06eeb34d28db49beae8862a3b0b
harddy0 <kingstonharddycanales@gmail.com>
2026-02-21 14:31:36 +0800

    asdww

---
+++ src/config/config copy.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'motorcycle_loan_db');
+define('DB_USER', 'root');
+define('DB_PASS', 'Password1');
+
+define('BASE_URL', 'http://localhost/ML-Motor-Loan-System');
+
+// Secondary Database (Optional)
+define('DB2_HOST', 'localhost');
+define('DB2_NAME', 'masterdata');
+define('DB2_USER', 'root');
+define('DB2_PASS', 'Password1');
